29f4aebb0c3b6f3c3186ad5886e7438baeeebe6a
Mehrdad Mehralian <dadwic@yahoo.com>
2020-08-26 12:33:40 +0430

    changes domain props

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,33 @@ import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import Divider from "@material-ui/core/Divider";
+import Tooltip from "@material-ui/core/Tooltip";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import ExploreIcon from "@material-ui/icons/Explore";
+import StarsIcon from "@material-ui/icons/Stars";
+import BlockIcon from "@material-ui/icons/Block";
 import { sentenceCase } from "sentence-case";
 import ProTip from "../src/ProTip";
 import Copyright from "../src/Copyright";
 import domains from "../public/domains.json";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
-    minWidth: 275,
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(5),
   },
-});
+  card: {
+    minWidth: 275,
+    backgroundColor: theme.palette.grey[50],
+  },
+  premium: {
+    color: theme.palette.warning.main,
+  },
+}));
 
 export default function Index(props) {
   const classes = useStyles();
@@ -37,15 +51,36 @@ export default function Index(props) {
           {sentenceCase(props.host, { delimiter: "." })}
         </Typography>
         <ProTip />
-        <Grid container spacing={1} justify="center">
+        <Typography variant="h5" color="textSecondary" gutterBottom>
+          All domains ({domains.length})
+        </Typography>
+        <Divider variant="inset" />
+        <Grid container spacing={1} justify="center" className={classes.root}>
           {orderBy(domains, "domain", "asc").map((d, i) => (
             <Grid item xs={12} md={4} key={i}>
-              <Card className={classes.root}>
+              <Card className={classes.card} variant="outlined">
                 <CardContent>
-                  <Typography variant="h4">
-                    {sentenceCase(d.domain, { delimiter: "." })}
-                  </Typography>
+                  <Typography variant="h5">{d.domain}</Typography>
                 </CardContent>
+                <CardActions>
+                  {d.premium && (
+                    <Tooltip title="Premium" placement="top">
+                      <StarsIcon className={classes.premium} />
+                    </Tooltip>
+                  )}
+                  {d.blocked && (
+                    <Tooltip title="Not for sale" placement="top">
+                      <BlockIcon color="error" />
+                    </Tooltip>
+                  )}
+                  <IconButton
+                    size="small"
+                    target="_blank"
+                    href={`http://${d.domain}`}
+                  >
+                    <ExploreIcon />
+                  </IconButton>
+                </CardActions>
               </Card>
             </Grid>
           ))}
